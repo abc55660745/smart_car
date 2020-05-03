e0587b0b145d22cd77f1a002c42b26c32c5677c3
zuo qingyu <zuoqingyu@nuaa.deu.cn>
2020-05-03 19:42:05 +0800

    重写了CCD处理函数，加了亿点点注释，新增了两个串口通道用来与多个软件通信

--- Src/main.c
+++ Src/main.c
@@ -40,23 +40,28 @@
 
 #define RXBUFFERSIZE  256
 
-char RxBuffer[RXBUFFERSIZE];
-uint8_t aRxBuffer;
-uint8_t Uart1_Rx_Cnt = 0;
-uint16_t direction = 0;
-char ad_flag = 1;
-char ccd_flag = 0;
-char zhijiao = 0;
-uint8_t ccd_s[128];
-uint8_t ccd_p[128];
-uint16_t ccd_count = 0;
-uint16_t ccd_SI = 1000;
+char RxBuffer[RXBUFFERSIZE];  //串口接收使用数组
+uint8_t aRxBuffer;            //同上
+uint8_t Uart1_Rx_Cnt = 0;     //串口接收计数
+uint16_t direction = 0;       //舵机PID值暂存
+char ad_flag = 1;             //ADC完成标志位（用于同步锁）
+char ccd_flag = 0;            //CCD的CLK电平记录，用于调控CLK输出
+char zhijiao = 0;             //暂时没啥用
+uint8_t ccd_s[128] = {0};     //CCD原始值记录
+uint8_t ccd_p[2][128] = {0};  //CCD处理值记录，二维数组保存上一次记录
+uint16_t ccd_count = 0;       //CCD的CLK输出计次，用于调控数组写入
+uint16_t ccd_SI = 1000;       //CCD曝光时间，单位为半个CLK周期
 
-uint8_t iii = 0;
-
+//通过串口2向匿名地面站发送函数，参数1为发送数组指针，参数2为发送数组大小
 void send(int16_t*, uint8_t);
+
+//ccd处理函数，包含二值化和舵机控制
 void ccd_process(void);
+
+//将CCD原始数据发送至CCD上位机软件
 void send_ccd(void);
+
+//CCD上位机通信使用
 void PutChar(unsigned char data);
 
 /* USER CODE END PTD */
@@ -123,8 +128,10 @@ int main(void)
   MX_TIM7_Init();
   MX_TIM1_Init();
   MX_TIM8_Init();
+  MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
-	//定时器和串口初始�?
+	//定时器和串口初始化
 	HAL_UART_Receive_IT(&huart1, (uint8_t *)&aRxBuffer, 1);
 	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
@@ -134,16 +141,16 @@ int main(void)
 	HAL_TIM_Base_Start_IT(&htim4);
 	HAL_TIM_Base_Start_IT(&htim7);
 	
+	//写入IO口初始值
 	HAL_GPIO_WritePin(SI_GPIO_Port, SI_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(CLK_GPIO_Port, CLK_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(PWM2_GPIO_Port, PWM2_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(PWM2_GPIO_Port, PWM4_Pin, GPIO_PIN_RESET);
-	//printf("start\n");
 	
-	for(;iii < 128; iii++)
-	{
-		ccd_s[iii] = 0;
-	}
+	//设置电机PWM（暂时没写差速
+	__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, 60);
+	__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_2, 60);
+	//printf("start\n");
 	
   /* USER CODE END 2 */
 
@@ -190,17 +197,18 @@ int main(void)
 //		  TIM3->CCR1 = pwmVal;     ?????
 		  HAL_Delay(20);
 		}*/
+		
+		//通过串口3直接以01发送CCD二值化以后的数据
 		char se[130];
+		uint8_t i;
 		HAL_Delay(1000);
-		for(iii = 0; iii < 128; iii++)
+		for(i = 0; i < 128; i++)
 		{
-			se[iii] = ccd_p[iii] + '0';
+			se[i] = ccd_p[0][i] + '0';
 		}
 		se[128] = '\n';
 		se[129] = '\r';
-		HAL_UART_Transmit(&huart1, (uint8_t *)se, 130,0xFFFF);
-		__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, 60);
-		__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_2, 60);
+		HAL_UART_Transmit(&huart3, (uint8_t *)se, 130,0xFFFF);
 		//printf("\ntt\n");
     
   }
@@ -250,6 +258,8 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+//CCD上位机通信使用
 void SendHex(unsigned char data)
 {
 	unsigned char temp;
@@ -273,6 +283,7 @@ void SendHex(unsigned char data)
 	}
 }
 
+//向CCD上位机发送CCD原始数据
 void send_ccd()
 {
 	uint8_t dataa[150], *data = dataa;
@@ -303,7 +314,7 @@ void send_ccd()
 	PutChar('#'); // 发送帧尾，一个字节
 }
 
-
+//串口回调函数
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
   /* Prevent unused argument(s) compilation warning */
@@ -324,16 +335,7 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 	else
 	{
 		RxBuffer[Uart1_Rx_Cnt++] = aRxBuffer;
-		//�����Ǵ�����Ϣ�������������޸�
-		/*
-		if((RxBuffer[Uart1_Rx_Cnt-1] == 0x0A)&&(RxBuffer[Uart1_Rx_Cnt-2] == 0x0D))
-		{
-			HAL_UART_Transmit(&huart1, (uint8_t *)&RxBuffer, Uart1_Rx_Cnt,0xFFFF);
-            while(HAL_UART_GetState(&huart1) == HAL_UART_STATE_BUSY_TX);
-			Uart1_Rx_Cnt = 0;
-			memset(RxBuffer,0x00,sizeof(RxBuffer));
-		}
-		*/
+		//匿名地面站数据解析程序
 		struct anotext t = char2ano(RxBuffer);
 		if(t.head == 0xaaaf && t.sum == Uart1_Rx_Cnt)
 		{
@@ -351,14 +353,15 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 			HAL_UART_Transmit(&huart1, (uint8_t *)sss, 7,0xFFFF);
 			free(sss);
 			delstr(send);
-			
 			//写入数据部分写在下面
+			
 		}
 	}
 	
 	HAL_UART_Receive_IT(&huart1, (uint8_t *)&aRxBuffer, 1);
 }
 
+//向匿名地面站发送数据，使用方法见上，需要包含structoperation
 void send(int16_t *in, uint8_t n)
 {
 	uint8_t i;
@@ -373,7 +376,7 @@ void send(int16_t *in, uint8_t n)
 	char *ss = ano2char(send);
 	for(i = 0; i < n * 2 + 4; i++)
 		ss[n * 2 + 4] += ss[i];
-	HAL_UART_Transmit(&huart1, (uint8_t *)ss, n * 2 + 5,0xFFFF);
+	HAL_UART_Transmit(&huart2, (uint8_t *)ss, n * 2 + 5,0xFFFF);
 	free(ss);
 	delstr(send);
 }
@@ -384,41 +387,38 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   if (htim == (&htim4))
   {
 	//定时器4中断函数
-		
-		
-		if(ccd_flag)
+		if(ccd_flag)  //CLK为1时将CLK写0，并将CCD计数+1
 		{
 			HAL_GPIO_WritePin(CLK_GPIO_Port, CLK_Pin, GPIO_PIN_RESET);
 			ccd_flag = 0;
 			ccd_count++;
-			if(ccd_count <= 128)
+			if(ccd_count <= 128)  //在CCD下降沿启动ADC转换
 			{
-				if(ad_flag)
+				if(ad_flag)  //根据ADC是否上锁决定是否调用ADC
 				{
 					HAL_ADC_Start_IT(&hadc2);
-					ad_flag = 0;
+					ad_flag = 0;  //ADC上锁（转换完成后解锁
 				}
 				else
 				{
-					ccd_s[ccd_count - 1] = 127;
+					ccd_s[ccd_count - 1] = 127;  //如果ADC上锁则直接写入中值
 				}
 			}
-			else if(ccd_count == 130)
+			else if(ccd_count == 130)  //计数130时ADC转换全部完成，调用下一步处理程序
 			{
 				ccd_process();
 			}
-			
-			if(ccd_count == ccd_SI)
+			if(ccd_count == ccd_SI)  //判断是否达到曝光时间启动下一个SI循环
 			{
 				ccd_count = 0;
 				HAL_GPIO_WritePin(SI_GPIO_Port, SI_Pin, GPIO_PIN_SET);
 			}
-			if(ccd_count == 1)
+			if(ccd_count == 1)  //SI写0
 			{
 				HAL_GPIO_WritePin(SI_GPIO_Port, SI_Pin, GPIO_PIN_RESET);
 			}
 		}
-		else
+		else  //CLK为0时写1
 		{
 			HAL_GPIO_WritePin(CLK_GPIO_Port, CLK_Pin, GPIO_PIN_SET);
 			ccd_flag = 1;
@@ -427,7 +427,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   }
 		else if (htim == (&htim7))
     {
-        //定时器7中断函数
+      //定时器7中断函数
+			/* 本段代码已废弃，这里以后要写PID程序
 			uint8_t count = 0, i;
 			int16_t sum = 0;
 			for(i = 5; i < 123; i++)
@@ -445,31 +446,39 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 				direction = sum * 0.7 + 68;
 				__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, direction);
 			}
+			*/
     }
 }
 
+//ADC中断回调函数
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
-	ad_flag = 1;
+	ad_flag = 1;  //ADC同步锁解锁
 	//HAL_ADC_Stop_IT(&hadc2); 
-	ccd_s[ccd_count - 1] = HAL_ADC_GetValue(&hadc2);
+	ccd_s[ccd_count - 1] = HAL_ADC_GetValue(&hadc2);  //将ADC数据写入CCD数组
 }
 
+//重定义printf使用
 void PutChar(unsigned char data)
 {
 	unsigned char *p = &data;
 	HAL_UART_Transmit(&huart1, (uint8_t *)p, 1, 0xffff);
 }
 
+//重定义printf使用
 int fputc(int ch, FILE *f)
 {
 	HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xffff);
   return ch;
 }
 
+//CCD数据处理函数
 void ccd_process()
 {
-	uint8_t max, min, i = 5, yuzhi;
+	uint8_t max, min, i = 5, j, yuzhi, count = 0, temp[128];
+	int16_t sum = 0;
+	
+	//循环寻找CCD原始数据中最大值与最小值
 	for(max = ccd_s[i], min = ccd_s[i]; i < 123; i++)
 	{
 		if(ccd_s[i] > max)
@@ -477,22 +486,66 @@ void ccd_process()
 		if(ccd_s[i] < min)
 			min = ccd_s[i];
 	}
-	yuzhi = (max + min) / 2;
+	
+	yuzhi = (max + min) / 2;  //阈值设为最大值与最小值的中值
+	
+	//对原始数据进行二值化处理
 	for(i = 0; i < 128; i++)
 	{
 		if(ccd_s[i] < yuzhi)
-			ccd_p[i] = 0;
+			ccd_p[0][i] = 0;
 		else
-			ccd_p[i] = 1;
+			ccd_p[0][i] = 1;
+	}
+	
+	//下面使用两次识别取并集降低干扰
+	//对本次数据与上次数据寻找并集，并将并集存在位置写入临时函数
+	for(i = 4; i < 124; i++)
+	{
+		if(ccd_p[0][i] == ccd_p[1][i] && ccd_p[0][i] == 0)
+		{
+			temp[count] = i;
+			count++;
+		}
+	}
+	
+	//将i,j设为并集中位数位置
+	i = temp[count / 2];
+	j = i;
+	count = 0;
+	//从并集中位数位置开始，在本次数据中向上寻找上升沿
+	for(;!ccd_p[0][i] && i <= 124; i++)
+	{
+		sum += i;
+		count++;
+	}
+	//从并集中位数位置开始，在本次数据中向下寻找下降沿
+	for(;!ccd_p[0][j] && j >= 4; j--)
+	{
+		sum += i;
+		count++;
+	}
+	//上述操作得到上次识别的黑线在本次识别中的位置（去除误识别的其他黑色
+	sum /= count;
+	sum -= 63;
+	//得到本次黑线的中间位置，使用简单计算换算为舵机PWM占空比并写入舵机
+	direction = sum * 0.6 + 68;
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, direction);
+	for(i = 0; i < 128; i++)
+	{
+		ccd_p[1][i] = ccd_p[0][i];  //将本次数据写入到上次位置，等待下一次读取
 	}
 }
 
+//重定义printf使用
 int fgetc(FILE *f)
 {
   uint8_t ch = 0;
   HAL_UART_Receive(&huart1, &ch, 1, 0xffff);
   return ch;
 }
+
+/* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
